cc0a17747be16ce8244ec560aa813006b31492ca
Iman Alfathan Yudhanto <iman.alfathan@ui.ac.id>
2019-01-04 00:25:31 +0700

    Perbaikan front end minor: Penambahan alert ketika data berhasil diedit.

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.example.demo.model.UserModel;
 import com.example.demo.model.BarangDipinjamModel;
 import com.example.demo.model.BarangModel;
@@ -82,9 +84,10 @@ public class PageController {
 	}
 
 	@PostMapping("/sipen/EditBarang/submit")
-	public String editBarangSubmit(Model model, @ModelAttribute BarangModel barangModel) {
+	public String editBarangSubmit(Model model, RedirectAttributes redirectAttributes, @ModelAttribute BarangModel barangModel) {
 		System.out.println("barang " + barangModel);
 		barangService.editBarang(barangModel);
+		redirectAttributes.addFlashAttribute("sukses","Data berhasil dirubah");
 		return "redirect:/sipen/EditBarang/" + barangModel.id;
 	}
 
